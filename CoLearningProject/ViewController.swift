ef9bea8f8a29a571df1278b436a455af89c52222
moxjet200 <judd_cobler@yahoo.com>
2014-12-04 09:22:04 -0600

    Initial Commit

---
+++ CoLearningProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoLearningProject
+//
+//  Created by Judd Cobler on 12/4/14.
+//  Copyright (c) 2014 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
